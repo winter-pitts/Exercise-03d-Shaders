678f7f17891097ecffdca3826f0ca985f68e00c9
winter-pitts <wmpitts@iu.edu>
2022-10-06 12:45:08 -0400

    Complete Exercise 3d

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -25,6 +25,10 @@ func update_time():
 
 func update_fever():
 	$Fever.value = Global.fever
+	var styleBox = $Fever.get("custom_styles/fg")
+	styleBox.bg_color.h = fever_h
+	styleBox.bg_color.s = (Global.fever / 100.0) * fever_s
+	styleBox.bg_color.v = (fever_v/2) + ((Global.fever / 100.0) * (fever_v/2)) 
 
 func update_lives():
 	var indicator_pos = Vector2(indicator_margin.x, Global.VP.y - indicator_margin.y)
